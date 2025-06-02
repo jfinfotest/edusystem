96fcc2020b87c778b6c5a1d2549ccce709e60a7c
Jhon <jfinfocesde@gmail.com>
2025-06-02 01:49:11 -0500

    02-06-2025

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"]
+  },
+  env: {
+    TZ: 'UTC',
+  }
 };
 
 export default nextConfig;
